87f156ba9b4fcc4959b5330f7e0054eb3bd2e39f
Unknown <marianopotetti@hotmail.com>
2018-05-12 15:52:01 -0300

    comitno s

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,7 @@ public class Main {
         // agregarInvitado. Agrego los fuegos individuales y packs que cree recien a la lista FuegosLista que tiene
         // ese evento. Finalmente invoco el metodo encenderVelas que tiene ese evento. Viva la chiqui conchuda.
         //
+        System.out.println("alapapa");
         System.out.println("ÿyyyyyyyyffff");
         System.out.println("flyflyfly");		
         System.out.println("smile");		
